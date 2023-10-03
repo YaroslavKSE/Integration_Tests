054c17f141b6b08b370997a2bdc132189145a587
YaroslavKSE <ikhomych@kse.org.ua>
2023-10-03 13:15:41 +0300

    Implemented a prediction mechanism based on a historical data for user count. API select from database date all online_count values which is equal to provided date hour and day and then calculates average value and returns a json object and api response 200. If there was no data it returns 0

--- api.py
+++ api.py
@@ -3,7 +3,6 @@ from flask_restful import Resource, Api
 import sqlite3
 import datetime
 
-
 app = Flask(__name__)
 api = Api(app)
 
@@ -13,7 +12,7 @@ DB_NAME = 'online_users.db'
 
 
 def str_to_datetime(date_str):
-    return datetime.datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S.%f%z')
+    return datetime.datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S')
 
 
 # Calculate the difference in seconds between two datetimes
@@ -64,6 +63,42 @@ class UsersOnlineResource(Resource):
 
 
 api.add_resource(UsersOnlineResource, "/api/stats/users")
+
+
+class UserPredictionResource(Resource):
+    @staticmethod
+    def get():
+        req_date = request.args.get('date')
+        if not req_date:
+            return {"error": "Date parameter is required."}, 400
+
+        req_datetime = datetime.datetime.strptime(req_date, '%Y-%m-%dT%H:%M:%S')
+        day_of_week = req_datetime.day
+        hour = req_datetime.hour
+
+        # Query the database for historical data for the given day and hour
+        conn = sqlite3.connect(DB_NAME)
+        cursor = conn.cursor()
+        day_str = req_datetime.strftime('%d')
+        hour_str = req_datetime.strftime('%H')
+
+        # Construct and execute the SQL query
+        cursor.execute('''SELECT online_count FROM user_stats
+                          WHERE strftime('%d', date) = ? AND strftime('%H', date) = ?''',
+                       (day_str, hour_str))
+
+        online_counts = [row[0] for row in cursor.fetchall()]
+
+        # Calculate the average
+        if not online_counts:
+            return {"onlineUsers": 0}, 200
+
+        avg_online_users = sum(online_counts) / len(online_counts)
+
+        return {"onlineUsers": round(avg_online_users)}, 200
+
+
+api.add_resource(UserPredictionResource, "/api/predictions/users")
 
 if __name__ == '__main__':
     app.run(debug=True)
